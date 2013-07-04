5656ec4e823920b233b120033ca3ddf5f4eaffff
Daniel Farrell <dfarrell07@gmail.com>
2013-07-04 02:57:57 -0400

    Planner can load strategies

--- planner/planner.py
+++ planner/planner.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
-import lib.log_lib as log_lib
+import json
+
+import lib.lib as lib
+
+STRAT_DIR = "planner/strategies/"
 
 class Planner:
 
-    def __init__(self):
-        self.logger = log_lib.get_logger()
+    def __init__(self, strat_file="strategy0.json"):
+        self.logger = lib.get_logger()
         self.logger.debug("Planner has logger")
+
+        self.strat = self.load_strategy(strat_file)
+        
+
+    def load_strategy(self, strat_file):
+        qual_strat_file = lib.prepend_prefix(STRAT_DIR + strat_file)
+        self.logger.info("Strategy file: " + qual_strat_file)
+
+        strat_fd = open(qual_strat_file)
+        self.strat = json.load(strat_fd)
+        self.logger.debug("Strategy: " + str(self.strat))

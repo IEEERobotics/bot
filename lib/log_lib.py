d2b1fb36f3d886ed3b23a7160c281e10a795496d
Daniel Farrell <dfarrell07@gmail.com>
2013-07-04 02:23:04 -0400

    Fixed conditional indent errors

--- lib/log_lib.py
+++ lib/log_lib.py
@@ -17,8 +17,8 @@ def get_logger(prefix=None):
 
     # Setup path to log output. Allows usage from any subpackage.
     if prefix is None:
-    prefix = "../" * getcwd().split("/bot")[1].count("/")
-    qual_log_file = prefix + LOG_FILE
+        prefix = "../" * getcwd().split("/bot")[1].count("/")
+        qual_log_file = prefix + LOG_FILE
 
     # Build logger
     logger = logging.getLogger(__name__)
@@ -27,22 +27,22 @@ def get_logger(prefix=None):
     # Check if log exists and should therefore be rolled
     needRoll = False
     if path.isfile(qual_log_file):
-    needRoll = True
+        needRoll = True
 
     # Build file output formatter
     file_formatter = logging.Formatter("%(asctime)s | %(levelname)s | "
-                                     "%(filename)s | %(funcName)s | "
-                                     "%(lineno)d | %(message)s")
+                                       "%(filename)s | %(funcName)s | "
+                                       "%(lineno)d | %(message)s")
 
     # Build stream output formatter
     stream_formatter = logging.Formatter("%(filename)s | %(funcName)s | "
-                                       "%(lineno)d | %(levelname)s | "
-                                       "%(message)s")
+                                         "%(lineno)d | %(levelname)s | "
+                                         "%(message)s")
 
     # Build file handler (for output log output to files)
     file_handler = logging.handlers.RotatingFileHandler(qual_log_file, mode="a",
-                                                      backupCount=50, 
-                                                      delay=True)
+                                                        backupCount=50, 
+                                                        delay=True)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(file_formatter)
 
@@ -57,7 +57,7 @@ def get_logger(prefix=None):
 
     # This is a stale log, so roll it
     if needRoll:
-    logger.handlers[0].doRollover()
+        logger.handlers[0].doRollover()
 
     logger.debug("Logger built")
     return logger

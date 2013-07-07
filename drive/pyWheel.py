05570bf649e5431a4e2c4224da4c555e3766e199
Daniel Farrell <dfarrell07@gmail.com>
2013-07-07 19:53:21 -0400

    Refactored pyWheel to subclass Driver

--- drive/pyWheel.py
+++ drive/pyWheel.py
@@ -1,70 +1,102 @@
 #!/usr/bin/env python
-# pass angle in degrees
-# Wheel A = front left
-# Wheel B = front Right
-# Wheel C = Back Left
-# Wheel D = Back Right
-	
-# Motion library to abstract wheel movement and macro moves from hardware.
+"""Pass low-level move commands to motors with mecanum wheels."""
+
 from math import sin, cos, pi
 from time import sleep
 
-def iowrite(wheel, ds):
-    print wheel, ds
-	
-def move(speed, angle, time=0):
-    """Calculating voltage multiplier for each wheel, passing to io pins.
+import driver
+import lib.lib as lib
 
-    :param speed: Magnitude of robot's translation speed.
-    :type speed: float.
-    :param angle: Angle at which robot should translate.
-    :type angle: float.
-    :param time: Time in seconds that robot should do this move.
-    :type time: float.
+
+class MechDriver(driver.Driver):
+
+    """Subclass of Driver for movement with mecanum wheels.
+
+    Motor A = front left
+    Motor B = front Right
+    Motor C = Back Left
+    Motor D = Back Right
 
     """
 
-    # Calculate voltage multipliers
-    front_left = speed * sin(angle*pi/180 + pi/4)
-    front_right = speed * cos(angle*pi/180 + pi/4)
-    back_left = speed * cos(angle*pi/180 + pi/4)
-    back_right = speed * sin(angle*pi/180 + pi/4)  
+    def __init__(self):
+        """Setup logger."""
 
-    # Write to io pins.
-    iowrite("front_left", front_left)
-    iowrite("front_right", front_right)
-    iowrite("back_left", back_left)
-    iowrite("back_right", back_right)
+        self.logger = lib.get_logger()
+        self.logger.debug("MechDriver has logger")
 
-    # Sleep for user defined amount of time
-    sleep(time)
+    def iowrite(self, motor, ds):
+        """Write to IO pens that control the motors.
 
-    # Stop robot movement.
-    iowrite("front_left", 0)
-    iowrite("front_right", 0)
-    iowrite("back_left", 0)
-    iowrite("back_right", 0)
-  
-def rotate(Rspeed, time=0):
-    """Controlling rotation of robot"""
+        TODO(dfarrell07): This is a stub
 
-    #Calculate voltage multipliers
-    front_left = Rspeed
-    front_right = -Rspeed
-    back_left = Rspeed
-    back_right = -Rspeed
+        :param motor: Motor to set speed of.
+        :param ds: Duty cycle that motor should be set to.
 
-    # Write to io pins.
-    iowrite("front_left", front_left)
-    iowrite("front_right", front_right)
-    iowrite("back_left", back_left)
-    iowrite("back_right", back_right)
+        """
+        self.logger.debug("IO write: motor: {}, ds: {}".format(motor, ds))
 
-    # Sleep for user defined amount of time
-    sleep(time)
+    def move(self, speed, angle, time=0):
+        """Calculate voltage multiplier for each motor, pass to io pins.
 
-    # Stop robot movement.
-    iowrite("front_left", 0)
-    iowrite("front_right", 0)
-    iowrite("back_left", 0)
-    iowrite("back_right", 0)
+        :param speed: Magnitude of robot's translation speed.
+        :type speed: float
+        :param angle: Angle in degrees at which robot should translate.
+        :type angle: float
+        :param time: Time in seconds that robot should do this move.
+        :type time: float
+
+        """
+        # Calculate voltage multipliers
+        front_left = speed * sin(angle*pi/180 + pi/4)
+        front_right = speed * cos(angle*pi/ 180 + pi/4)
+        back_left = speed * cos(angle*pi/ 180 + pi/4)
+        back_right = speed * sin(angle*pi/180 + pi/4)
+
+        # Write to io pins.
+        iowrite("front_left", front_left)
+        iowrite("front_right", front_right)
+        iowrite("back_left", back_left)
+        iowrite("back_right", back_right)
+
+        # Sleep for user defined amount of time
+        sleep(time)
+
+        # Stop robot movement.
+        iowrite("front_left", 0)
+        iowrite("front_right", 0)
+        iowrite("back_left", 0)
+        iowrite("back_right", 0)
+
+    def rotate(self, Rspeed, time=None):
+        """Control rotation of robot.
+
+        Note that the time param is designed for testing, especially
+        via the CLI, as it may be difficult to fire a move and stop command
+        as quickly as would be desired.
+
+        :param Rspeed: Desired rotational speed.
+        :param time: Number of seconds to force rotation.
+
+        """
+        #Calculate voltage multipliers
+        front_left = Rspeed
+        front_right = -Rspeed
+        back_left = Rspeed
+        back_right = -Rspeed
+
+        # Write to io pins.
+        iowrite("front_left", front_left)
+        iowrite("front_right", front_right)
+        iowrite("back_left", back_left)
+        iowrite("back_right", back_right)
+
+        # Sleep for user defined amount of time
+        if time is not None:
+            sleep(time)
+
+        # Stop robot movement.
+        iowrite("front_left", 0)
+        iowrite("front_right", 0)
+        iowrite("back_left", 0)
+        iowrite("back_right", 0)

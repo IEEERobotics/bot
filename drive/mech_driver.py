4b34425d14988a252cd36d3fd7ac62c47d7c7529
Daniel Farrell <dfarrell07@gmail.com>
2013-07-07 21:32:28 -0400

    Bugfixes, cleanup

--- drive/mech_driver.py
+++ drive/mech_driver.py
@@ -11,6 +11,8 @@ class MechDriver(driver.Driver):
 
     """Subclass of Driver for movement with mecanum wheels.
 
+    TODO(dfarrell07): Override Driver.move with translate + rotate combo code.
+
     Motor A = front left
     Motor B = front Right
     Motor C = Back Left
@@ -30,13 +32,15 @@ class MechDriver(driver.Driver):
         TODO(dfarrell07): This is a stub
 
         :param motor: Motor to set speed of.
+        :type ds: string
         :param ds: Duty cycle that motor should be set to.
+        :type ds: float
 
         """
         self.logger.debug("IO write: motor: {}, ds: {}".format(motor, ds))
 
-    def move(self, speed, angle):
-        """Calculate voltage multiplier for each motor, pass to io pins.
+    def translate(self, speed, angle):
+        """Calculate voltage multiplier for each motor, pass to IO pins.
 
         :param speed: Magnitude of robot's translation speed.
         :type speed: float
@@ -44,32 +48,37 @@ class MechDriver(driver.Driver):
         :type angle: float
 
         """
+        self.logger.debug("Translate speed: {}, angle: {}".format(speed, angle))
+
         # Calculate voltage multipliers
         front_left = speed * sin(angle*pi/180 + pi/4)
         front_right = speed * cos(angle*pi/ 180 + pi/4)
         back_left = speed * cos(angle*pi/ 180 + pi/4)
         back_right = speed * sin(angle*pi/180 + pi/4)
 
-        # Write to io pins.
-        iowrite("front_left", front_left)
-        iowrite("front_right", front_right)
-        iowrite("back_left", back_left)
-        iowrite("back_right", back_right)
+        # Write to IO pins.
+        self.iowrite("front_left", front_left)
+        self.iowrite("front_right", front_right)
+        self.iowrite("back_left", back_left)
+        self.iowrite("back_right", back_right)
 
-    def rotate(self, Rspeed):
+    def rotate(self, rotate_speed):
         """Control rotation of robot.
 
-        :param Rspeed: Desired rotational speed.
+        :param rotate_speed: Desired rotational speed.
+        :type rotate_speed: float
 
         """
-        #Calculate voltage multipliers
-        front_left = Rspeed
-        front_right = -Rspeed
-        back_left = Rspeed
-        back_right = -Rspeed
+        self.logger.debug("Rotate speed: {}".format(rotate_speed))
 
-        # Write to io pins.
-        iowrite("front_left", front_left)
-        iowrite("front_right", front_right)
-        iowrite("back_left", back_left)
-        iowrite("back_right", back_right)
+        #Calculate voltage multipliers
+        front_left = rotate_speed
+        front_right = -rotate_speed
+        back_left = rotate_speed
+        back_right = -rotate_speed
+
+        # Write to IO pins.
+        self.iowrite("front_left", front_left)
+        self.iowrite("front_right", front_right)
+        self.iowrite("back_left", back_left)
+        self.iowrite("back_right", back_right)

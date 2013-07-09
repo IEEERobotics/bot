f648da40e0eb67d67e4d13abf68760389a708108
Neal O'Hara <ngo1991@gmail.com>
2013-07-09 19:56:35 -0400

    deleled old files

--- ir/MSP430 sample project files/msp430g2xx2_P1_04/Debug/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-CMD_SRCS += \
-../lnk_msp430g2332.cmd 
-
-

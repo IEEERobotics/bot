333fd93162f551197c3dead959225211b50ce67b
Daniel Farrell <dfarrell07@gmail.com>
2013-07-09 20:51:08 -0400

    Minor style fixes (PEP8)

--- scripts/check_pep8.sh
+++ scripts/check_pep8.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-pep8 start.py lib/lib.py planner/planner.py driver/driver.py gunner/gunner.py driver/mech_driver.py
+
+# Check source file for PEP8 conformance.
+
+pep8 start.py \
+     lib/lib.py \
+     planner/planner.py \
+     driver/driver.py \
+     gunner/gunner.py \
+     gunner/wheel_gunner.py \
+     driver/mech_driver.py \
+     localizer/localizer.py \
+     follower/follower.py \
+     lib/exceptions.py
+     

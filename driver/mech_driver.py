4677ef6bd10ee877cd58ac221c8a77748fe84a2f
Ahmed Samara <asamara@ncsu.edu>
2013-07-30 19:53:58 -0400

    Fixed pep8 violations (really)

--- driver/mech_driver.py
+++ driver/mech_driver.py
@@ -23,7 +23,7 @@ class MechDriver(driver.Driver):
     def __init__(self):
         """Run superclass's init."""
         super(MechDriver, self).__init__()
-    	max_speed = 10
+        max_speed = 10
 
     def iowrite(self, motor, ds):
         """Write to IO pens that control the motors.
@@ -34,10 +34,8 @@ class MechDriver(driver.Driver):
         :type ds: string
         :param ds: Duty cycle that motor should be set to.
         :type ds: float
-
         """
         self.logger.debug("IO write: motor: {}, ds: {}".format(motor, ds))
-		
 
     def basic_move(self, speed, angle, rotate_speed):
         """Build low-level commands for holonomic translations with rotations.
@@ -54,53 +52,63 @@ class MechDriver(driver.Driver):
                                                             angle,
                                                             rotate_speed))
 
+        # Calculate wheel speed ratios
+        # note: This is not the same as the car speed.
+        front_left = speed * sin(angle * pi / 180 + pi / 4) + rotate_speed
+        front_right = speed * cos(angle * pi / 180 + pi / 4) - rotate_speed
+        back_left = speed * cos(angle * pi / 180 + pi / 4) + rotate_speed
+        back_right = speed * sin(angle * pi / 180 + pi / 4) - rotate_speed
 
-        # Calculate speed ratios
-        front_left = speed * sin(angle*pi/180 + pi/4) + rotate_speed
-        front_right = speed * cos(angle*pi/ 180 + pi/4) - rotate_speed
-        back_left = speed * cos(angle*pi/ 180 + pi/4) + rotate_speed
-        back_right = speed * sin(angle*pi/180 + pi/4) - rotate_speed
-		
-	#Calculate duty cycle (ratio to max_speed)
-	#Todo for Ahmed: Run tests to find out what max_speed should be, if it should just be 'speed'.
-	front_left_ds = floor(front_left / max_speed * 100)
-	front_right_ds = floor(front_right / max_speed * 100)
-	back_left_ds = floor(back_left / max_speed * 100)
-	back_right_ds = floor(back_right / max_speed * 100) 
-		
-	#Prevent invalid duty cycle values.
-	if front_left_ds > 100:
-	  front_left_ds = 100
-	if front_right_ds >100:
-	  front_right_ds = 100
-	if back_left_ds > 100:
-	  back_left_ds = 100
-	if back_right_ds > 100:
-	  back_right_ds = 100
-	  
-	if front_left_ds < 0:
-	  front_left_ds = 0
-	if front_right_ds <0:
-	  front_right_ds = 0
-	if back_left_ds < 0:
-	  back_left_ds = 0
-	if back_right_ds < 0:
-	  back_right_ds = 0
-		 
-		
-	#Determine direction of wheel.
-	#Bool will determine value of direction pin.
-	if front_left > 0:
-	  front_left_forward = True
-	if front_right > 0:
-	  front_right_forward = True
-	if back_left > 0:
-	  back_left_forward = True
-	if back_right > 0:
-	  back_right_foward = True
- 
+        # Calculate duty cycle (ratio to max_speed)
+        # Todo for Ahmed: Run tests to find out what max_speed should be.
+        # I think it's 3.3 (max output voltage) but check.
+        front_left_ds = floor(front_left / max_speed * 100)
+        front_right_ds = floor(front_right / max_speed * 100)
+        back_left_ds = floor(back_left / max_speed * 100)
+        back_right_ds = floor(back_right / max_speed * 100)
+
+        # Prevent invalid duty cycle values.
+        if front_left_ds > 100:
+            front_left_ds = 100
+        if front_right_ds > 100:
+            front_right_ds = 100
+        if back_left_ds > 100:
+            back_left_ds = 100
+        if back_right_ds > 100:
+            back_right_ds = 100
+
+        # Prevent invalid less than zero duty cycles.
+        if front_left_ds < 0:
+            front_left_ds = 0
+        if front_right_ds < 0:
+            front_right_ds = 0
+        if back_left_ds < 0:
+            back_left_ds = 0
+        if back_right_ds < 0:
+            back_right_ds = 0
+
+        # Determine direction of wheel.
+        # Bool will determine value of direction pin.
+        if front_left > 0:
+            front_left_forward = True
+        else:
+            front_left_forward = False
+        if front_right > 0:
+            front_right_forward = True
+        else:
+            front_right_forward = False
+
+        if back_left > 0:
+            back_left_forward = True
+        else:
+            back_left_forward = False
+        if back_right > 0:
+            back_right_foward = True
+        else:
+            back_right_foward = False
+
         # Write to IO pins.
-        self.iowrite("front_left", front_left_ds)
-        self.iowrite("front_right", front_right_ds)
-        self.iowrite("back_left", back_left_ds)
-        self.iowrite("back_right", back_right_ds)
+        self.iowrite("front_left", front_left_ds, front_left_forward)
+        self.iowrite("front_right", front_right_ds, front_right_forward)
+        self.iowrite("back_left", back_left_ds, back_left_forward)
+        self.iowrite("back_right", back_right_ds, back_right_foward)

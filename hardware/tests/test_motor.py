20f4c0a9a020d31a36e5a4fdf15efd7570e37b03
Daniel Farrell <dfarrell07@gmail.com>
2013-08-14 22:28:10 -0400

    Expanded motor speed tests, added direction tests

--- hardware/tests/test_motor.py
+++ hardware/tests/test_motor.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import unittest
+from random import randint
 
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
@@ -15,13 +16,91 @@ except ImportError:
 # Build logger
 logger = lib.get_logger()
 
+
 class TestSpeed(unittest.TestCase):
+    """Test setting and checking the speed of a motor."""
 
     def setUp(self):
-        logger.debug("In setUp")
-        self.motor = m_mod.Motor(0, testing=True)
+        """Build motor object."""
+        self.m_num = 0
+        self.motor = m_mod.Motor(self.m_num, testing=True)
         logger.debug("Built {}".format(self.motor))
 
     def test_off(self):
+        """Test turning the motor off."""
         self.motor.speed = 0
-        print "In test_off"
+        assert self.motor.speed is 0
+
+    def test_full(self):
+        """Test turning the motor to 100% duty cycle."""
+        self.motor.speed = 100
+        assert self.motor.speed is 100
+
+    def test_half(self):
+        """Test the motor at half speed."""
+        self.motor.speed = 50
+        assert self.motor.speed is 50
+
+    def test_accel(self):
+        """Test a series of increasing speeds."""
+        for speed in range (0, 100, 5):
+            self.motor.speed = speed
+            assert self.motor.speed is speed
+
+    def test_manually_confirm(self):
+        """Test a series of random speeds, read the simulated HW to confirm."""
+        config = lib.load_config()
+
+        for i in range(10):
+            cur_speed = randint(0, 100)
+            self.motor.speed = cur_speed
+            with open(config["test_base_dir"] + str(self.m_num) + 
+                                                '/duty_ns', 'r') as f:
+                assert int(f.read()) is cur_speed
+
+    def test_over_max(self):
+        """Test speed over max speed. Should use maximum."""
+        self.motor.speed = 101
+        assert self.motor.speed is 100
+
+    def test_under_min(self):
+        """Test speed under minimum speed. Should use minimum."""
+        self.motor.speed = -1
+        assert self.motor.speed is 0
+
+
+class TestDirection(unittest.TestCase):
+    """Test setting and checking the direction of a motor."""
+
+    def setUp(self):
+        """Build motor object."""
+        self.m_num = 0
+        self.motor = m_mod.Motor(self.m_num, testing=True)
+        logger.debug("Built {}".format(self.motor))
+
+    def test_forward(self):
+        """Test motor in foward direction using text and int syntax."""
+        self.motor.direction = 1
+        assert self.motor.direction is 1
+        self.motor.direction = "foward"
+        assert self.motor.direction is 1
+
+    def test_reverse(self):
+        """Test motor in reverse direction using text and int syntax."""
+        self.motor.direction = 0
+        assert self.motor.direction is 0
+        self.motor.direction = "reverse"
+        assert self.motor.direction is 0
+
+    def test_invalid(self):
+        """Test a series of invalid directions."""
+        # First set a valid value so state is known
+        self.motor.direction = 1
+        self.motor.direction = 2
+        assert self.motor.direction is 1
+        self.motor.direction = -1
+        assert self.motor.direction is 1
+        self.motor.direction = "wrong"
+        assert self.motor.direction is 1
+        self.motor.direction = ""
+        assert self.motor.direction is 1

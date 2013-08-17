9cb70c58110417637c03a8d289cd4514627a841d
Daniel Farrell <dfarrell07@gmail.com>
2013-08-17 16:16:45 -0400

    Added hardware support to wheel gunner

--- gunner/wheel_gunner.py
+++ gunner/wheel_gunner.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 """Logic for firing using a wheel-based design."""
 
+from time import sleep
+
 import lib.lib as lib
 import gunner
+import hardware.motor as motor
+import hardware.solenoid as solenoid
 
 
 class WheelGunner(gunner.Gunner):
@@ -12,6 +16,14 @@ class WheelGunner(gunner.Gunner):
     def __init__(self):
         """Run superclass's init."""
         super(WheelGunner, self).__init__()
+
+        # Build solenoid for advancing dart
+        self.dart_sol = solenoid.Solenoid(0)
+
+        # Build motors for driving wheels
+        self.motors = []
+        self.motors.append(motor.Motor(0))
+        self.motors.append(motor.Motor(1))
 
     def fire(self, cmd):
         """Handle normal fire commands.
@@ -29,16 +41,24 @@ class WheelGunner(gunner.Gunner):
         self.advance_dart()
         self.logger.info("Fired dart")
 
-    def update_rotate_speed(self, rotate_speed=100):
-        """Accept and handle updates to wheel rotation speed.
+    @property
+    def wheel_speed(self):
+        """Getter for wheel rotation speed. Speed is % of max."""
+        assert self.motors[0].speed == self.motors[1].speed, "{} != {}".format(
+            self.motors[0].speed, self.motors[1].speed)
+        return self.motors[0].speed
 
-        TODO(dfarrell07): This is a stub.
+    @wheel_speed.setter
+    def wheel_speed(self, speed=100):
+        """Setter for updates to wheel rotation speed.
 
-        :param rotate_speed: Desired speed of wheel rotation (% of max).
-        :type rotate_speed: float
+        :param speed: Desired speed of wheel rotation (% of max).
+        :type speed: int
 
         """
-        self.logger.debug("New rotate speed: {}".format(rotate_speed))
+        for motor in self.motors:
+            motor.speed = speed
+        self.logger.debug("New wheel speed: {}".format(speed))
 
     def advance_dart(self):
         """Cause hardware to push a dart into the spinning wheels.
@@ -47,3 +67,6 @@ class WheelGunner(gunner.Gunner):
 
         """
         self.logger.debug("Advancing dart")
+        self.dart_sol.extend()
+        sleep(.5)
+        self.dart_sol.retract()

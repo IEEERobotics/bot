c44764f8d434fa931d7916b8b02e891cb8edd392
Daniel Farrell <dfarrell07@gmail.com>
2013-08-17 16:16:02 -0400

    Raise NotImplementedError if move not overriden by child

--- driver/driver.py
+++ driver/driver.py
@@ -20,10 +20,12 @@ class Driver(object):
     def move(self, desc):
         """Accept and handle a movement command.
 
-        TODO(dfarrell07): This is a stub.
+        This method is not meant to be called, but instead is meant to show
+        that subclasses should override it in their implementation.
 
         :param desc: Description of movement command to handle.
         :type desc: string
 
         """
-        self.logger.debug("Mv cmd: {}".format(desc))
+        self.logger.error("The move method must be overridden by a subclass.")
+        raise NotImplementedError("Subclass must override this method.")

dbe1b2f121572f129087b9528db726ce6e2db6e7
Daniel Farrell <dfarrell07@gmail.com>
2013-08-18 04:48:39 -0400

    Convert turret to store servos in dict

--- hardware/turret.py
+++ hardware/turret.py
@@ -3,6 +3,8 @@
 import lib.lib as lib
 import hardware.servo as servo
 
+SERVO_X_ID = 0
+SERVO_Y_ID = 1
 
 class Turret(object):
 
@@ -14,13 +16,14 @@ class Turret(object):
         self.logger = lib.get_logger()
 
         # Build and store abstraction of servos for x and y axis movement
-        self.servo_x = servo.Servo(0)
-        self.servo_y = servo.Servo(1)
+        self.servos = {"servo_x": servo.Servo(SERVO_X_ID), 
+                       "servo_y": servo.Servo(SERVO_Y_ID)
+                        }
 
     def __str__(self):
         """Represent turret in a human-readable way."""
-        return "Turret: servo_x: {}, servo_y: {}".format(self.servo_x,
-                                                         self.servo_y)
+        return "Turret: servo_x: {}, servo_y: {}".format(self.servos["servo_x"],
+                                                         self.servos["servo_y"])
 
     @property
     def x_angle(self):
@@ -29,7 +32,7 @@ class Turret(object):
         :returns: Angle in degrees of turret on x-axis.
 
         """
-        return self.servo_x.position
+        return self.servos["servo_x"].position
 
     @x_angle.setter
     def x_angle(self, angle):
@@ -38,7 +41,7 @@ class Turret(object):
         :param angle: Angle in degrees to set turret to on x-axis.
 
         """
-        self.servo_x.position = angle
+        self.servos["servo_x"].position = angle
 
     @property
     def y_angle(self):
@@ -47,7 +50,7 @@ class Turret(object):
         :returns: Angle in degrees of turret on y-axis.
 
         """
-        return self.servo_y.position
+        return self.servos["servo_y"].position
 
     @y_angle.setter
     def y_angle(self, angle):
@@ -56,4 +59,4 @@ class Turret(object):
         :param angle: Angle in degrees to set turret to on y-axis.
 
         """
-        self.servo_y.position = angle
+        self.servos["servo_y"].position = angle

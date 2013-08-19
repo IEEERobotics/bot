6dd3edc6b641c9208e07c1cbeaf6bed56eb50420
Neal <neal@neal-Latitude-D620.(none)>
2013-08-19 13:15:55 -0400

    minor changes to test commands

--- ir/I2C_connect/infinite_loop_master.py
+++ ir/I2C_connect/infinite_loop_master.py
@@ -1,11 +1,14 @@
-
+import time
 from Adafruit_I2C import Adafruit_I2C
 
 slave = 0x90		# address of first slave, for now need to manually handle additional slaves
- 
 i2c = Adafruit_I2C(slave)
 
+P9_19: I2C2, SCL
+P9_20: I2C2, SDA
+
 count = 0
+recieved_byte = 0
 
 while True:
 	
@@ -13,5 +16,6 @@ while True:
 	
 	
 	
-	
-	print count recieved_byte
+	print(count, recieved_byte)
+	time.sleep(0.1)
+

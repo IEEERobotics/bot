45771b6b136f366e19b8f9894cdad34cdb51ba6b
Alwyn Smith <alwyn.smith@gmail.com>
2013-09-19 16:56:09 -0400

    Added docstrings to all methods

--- planner/fsm_planner.py
+++ planner/fsm_planner.py
@@ -2,6 +2,7 @@
 
 import string
 import sys
+    #TODO: (avsmith5) Switch print statements to logging
 
 
 class StateTable(object):
@@ -10,6 +11,7 @@ class StateTable(object):
     obj = None
 
     def __init__(self):
+        """Initializes the StateTable with expected values at startup"""
         self.start = False
         self.lineFound = True
         self.blueFound = False
@@ -19,11 +21,16 @@ class StateTable(object):
         self.lastHeading = 180  # tracks the last heading for navigation
         self.currentHeading = 180
         self.intersections = 0
-        self.positions = 0 # firing positions tracker
+        self.positions = 0  # firing positions tracker
         self.shotsTaken = 0
 
     @classmethod
     def getInstance(cls):
+        """Return the StateTable or instantiates it if it does not exist
+
+        :returns: cls.obj
+
+        """
         if cls.obj is None:
             cls.obj = StateTable()
         return cls.obj
@@ -33,6 +40,7 @@ class State(object):
     """Provide an interface for the State subclasses"""
 
     def __init__(self):
+        """Ensures that all State subclasses have the StateTable"""
         self.stateTable = StateTable.getInstance()
 
     def run(self):
@@ -46,9 +54,13 @@ class StateMachine:
     """Initializes and execute states, and manage transitions"""
 
     def __init__(self, initialState):
+        """Initializes the current the current state to the initial state """
         self.currentState = initialState
 
     def runAll(self):
+        """Executes the current state's run behaviors and transitions to
+        the next state until the finish state occurs.
+        """
         while (self.currentState != self.finish):
             Robot.test.run()  # for testing only
             self.currentState.run()
@@ -63,9 +75,13 @@ class TestStates(State):
     f = open("./tests/input/RobotNormalTest.txt")
 
     def __init__(self):
+        """Initializes the Test State as a subclass of State,
+        which ensures access to the StateTable
+        """
         super(TestStates, self).__init__()
-    #TODO: (avsmith5) Switch print statements to logging
+
     def run(self):
+        """Modifies the state table based on text file inputs"""
         input = self.f.readline()
         testCase = input.strip()
         print "Next Test Case: " + testCase
@@ -83,6 +99,7 @@ class TestStates(State):
             self.stateTable.centered = True
 
     def next(self):
+        """Does Nothing"""
         print "Test Next"
 
 
@@ -90,15 +107,19 @@ class WaitingForStart(State):
     """Waits for the course remote start signal after startup"""
 
     def __init__(self):
+        """Initializes WaitingforStart"""
         super(WaitingForStart, self).__init__()
-    #TODO: (avsmith5) Docstrongs on methods
+
     def run(self):
+        """Checks for the remote start signal
+        by calling the monitor behavior"""
         if self.stateTable.start is False:
             print "Execute: Still waiting\n"
         else:
             print "Execute: Starting up!\n"
 
     def next(self):
+        """Returns to the waiting state or transitions to the jerk state"""
         if self.stateTable.start is False:
             print "Transition to next State: Waiting\n"
             return Robot.waiting
@@ -111,12 +132,15 @@ class Jerk(State):
     """Execute rote behavior to move out of the start box"""
 
     def __init__(self):
+        """Initializes Jerk"""
         super(Jerk, self).__init__()
 
     def run(self):
+        """Calls the jerk behavior"""
         print "Execute: Rote initial move.\n"
 
     def next(self):
+        """Transitions to line seeking"""
         print "Transition to next State: FindLine\n"
         return Robot.findLine
 
@@ -125,9 +149,11 @@ class FindLine(State):
     """Checks for a line to follow or calls a rescue behavior"""
 
     def __init__(self):
+        """Initializes FindLine"""
         super(FindLine, self).__init__()
 
     def run(self):
+        """Confirms the presence of a line or calls a rescue behavior"""
         if self.stateTable.lineFound is False:
             #oscillate
             print "Execute: Looking for line.\n"
@@ -136,6 +162,7 @@ class FindLine(State):
             pass
 
     def next(self):
+        """Transitions to line following"""
         print "Transition to next State: Following\n"
         return Robot.following
 
@@ -144,12 +171,15 @@ class Following(State):
     """Follows a line until an event is indicated"""
 
     def __init__(self):
+        """Initializes Following"""
         super(Following, self).__init__()
 
     def run(self):
+        """Calls a line following behavior """
         print "Execute: Following line.\n"
 
     def next(self):
+        """Transitions to centering state or line finding"""
         #LostLine --> Stop/FindLine
         if self.stateTable.lineFound is False:
             return Robot.findLine
@@ -180,15 +210,21 @@ class Following(State):
 
 class CenterAndAlign(State):
     """Calls methods to center over blocks and intersections.
-    Calls methods to align with the course"""
+    Calls methods to align with the course
+    """
 
     def __init__(self):
+        """Initializes CenterAndAlign"""
         super(CenterAndAlign, self).__init__()
 
     def run(self):
+        """Calls a centering and alignment behavior
+        to ensure correct course orientation
+        """
         print("Execute: Center on Block. Aligned to Course.\n")
 
     def next(self):
+        """Transitions to choosing a new direction, firing, or finish """
         if self.stateTable.xFound is True:
             print "Transition to next State: Choose Direction\n"
             return Robot.chooseDir
@@ -207,13 +243,17 @@ class Firing(State):
     """Firing calls methods to aim and fire the turret"""
 
     def __init__(self):
+        """Initializes Firing"""
         super(Firing, self).__init__()
 
     def run(self):
+        """Calls the firing behavior. Increments the shot counter."""
+        #TODO (PaladinEng): Passing firing position index for firing solution?
         print "Execute: One shot = One kill.\n"
         self.stateTable.shotsTaken += 1
 
     def next(self):
+        """Transitions to choosing a new direction"""
         print "Transition to next State: ChooseDirection\n"
         return Robot.chooseDir
 
@@ -222,10 +262,11 @@ class ChooseDirection(State):
     """ChooseDirection determines the correct course heading after each stop"""
 
     def __init__(self):
+        """Initializes ChooseDirection"""
         super(ChooseDirection, self).__init__()
 
     def run(self):
-
+        """Determines the correct direction to move next"""
         self.stateTable.lastHeading = self.stateTable.currentHeading
 
         # Calculate a new heading
@@ -254,6 +295,7 @@ class ChooseDirection(State):
         print "Execute: Direction Set\n"
 
     def next(self):
+        """Transitions to line following"""
         print "Transition to next State: Following\n"
         return Robot.following
 
@@ -262,12 +304,15 @@ class Finish(State):
     """Finish is the final waiting state for the robot"""
 
     def __init__(self):
+        """Initializes Finish"""
         super(Finish, self).__init__()
 
     def run(self):
+        """Executes shutdown tasks then performs busy waiting"""
         print "All Done. Shut me off."
 
     def next(self):
+        """Stays in the finish state"""
         print "Transition to next State: Finish"
         return Robot.finish
 
@@ -276,6 +321,9 @@ class Robot(StateMachine):
     """Robot is the wrapper for the state classes and the state machine"""
 
     def __init__(self):
+        """Initializes Robot into the waiting state and begins cycling for
+        state behaviors and transitions
+        """
         StateMachine.__init__(self, Robot.waiting)
         self.runAll()
 

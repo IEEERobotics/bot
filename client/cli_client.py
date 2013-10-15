abb8c85cf2225e6045a7648c6aaa1a53061de378
Daniel Farrell <dfarrell07@gmail.com>
2013-10-15 18:15:55 -0400

    Added basic stub for CLI client

---
+++ client/cli_client.py
@@ -0,0 +1,11 @@
+"""Client for sending commands to the bot's server via CLI."""
+
+import lib.lib as lib
+import server.server as server
+
+class CLIClient(object):
+
+    """Send user commands to server running on the bot."""
+
+    def __init__(self):
+        pass

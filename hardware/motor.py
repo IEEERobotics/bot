4a6f8b518dcfec19d6b83a9b1d1a1f704be78c45
Arpan Chakraborty <napratin@yahoo.co.in>
2013-11-01 19:12:58 -0400

    Minor fix in motor: speed setter now converts incoming number to int

--- hardware/motor.py
+++ hardware/motor.py
@@ -118,12 +118,13 @@ class Motor(object):
         :type speed: float
 
         """
-        if speed > 100.0:
-            self.logger.warn("Invalid speed {}, using 100.0".format(speed))
-            speed = 100.0
-        elif speed < 0.0:
-            self.logger.warn("Invalid speed {}, using 0.0".format(speed))
-            speed = 0.0
+        speed = int(round(speed))
+        if speed > 100:
+            self.logger.warn("Invalid speed {}, using 100".format(speed))
+            speed = 100
+        elif speed < 0:
+            self.logger.warn("Invalid speed {}, using 0".format(speed))
+            speed = 0
 
         self.pwm.duty = int(round((speed / 100.) * self.pwm.period))
         self.logger.debug("Updated speed {}".format(self))

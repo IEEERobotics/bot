dab039aeb69b03b3e7260d15bce583285528d884
Daniel Farrell <dfarrell07@gmail.com>
2013-11-12 21:37:54 -0500

    Fixed many style/PEP8 issues in SimpleFollower, but it still has problems and may not actually be developed

--- follower/simple_follower.py
+++ follower/simple_follower.py
@@ -1,4 +1,4 @@
-"""A earlier version of the line follower."""
+"""A early version of the line follower."""
 
 import hardware.ir_hub as ir_hub_mod
 import lib.lib as lib
@@ -7,7 +7,7 @@ import driver.mec_driver as driver_mod
 
 class SimpleFollower(object):
 
-    """A earlier version of the line follower."""
+    """A early version of the line follower."""
 
     def __init__(self):
         """Setup driver, logger, ir_hub, and config."""
@@ -15,70 +15,63 @@ class SimpleFollower(object):
         self.ir_hub = ir_hub_mod.IRHub()
         self.driver = driver_mod.MecDriver()
         self.config = lib.load_config()
-        
+
     def overall_move(self):
-        """constant loop for the movement informatinon."""
-        while(True):
+        """Constant loop for the movement information."""
+        while True:
             dic = self.ir_hub.read_all_arrays()
-            if( on_line(dic["front"], dic["back"] ):
+            if on_line(dic["front"], dic["back"]):
                 rotate_correct()
             else:
                 move()
-        
-    def move(self, array_dictionary):
-        """ receives the dictionary of IR arrays of 
-        returns a (speed, angle) tuple.
-        
-        :param array_dictionary: a dictionary containg 4 arrays
-        which hold the ir sensor informatoin.
-        :returns: a speed and angle. 
-        
-        """
-	
-	front = array_dictionary()["front"]
-        switch: 
-            case 0: # mid and centered
-                return(100, 0)
-            case 1: # right and centered
-                return(80, 30)
-            case 2: # left and centered 
-                return(80, -30)
-            default:
-                # case not covered stop 
-     
-     
-                
-    def slope_between(self, point_a, point_b):
-        """ determines the slope between the two points
-        the mesaurement should come from off the physical bot. 
-        
-        :point_a:
-        :point_b:
-        """
-        array_space = 1 # needs to vary somewhat as the goes further away
-        
-        return (ya - yb)/(xa - xb)
-	# make a method to determine if the bot is off center
 
+    def move(self, array_dictionary):
+        """Receives the dict of IR arrays of returns a (speed, angle) tuple.
+
+        :param array_dictionary: Dicty with 4 IR arrays
+        :returns: A speed and angle
+
+        """
+
+        front = array_dictionary()["front"]
+        if front == 0:
+            # Mid and centered
+            return (100, 0)
+        elif front == 1:
+            # Right and centered
+            return (80, 30)
+        elif front == 2:
+             # Left and centered
+            return(80, -30)
+
+    def slope_between(self, point_a, point_b):
+        """Determines the slope between two points.
+
+        The measurement should come from the physical bot.
+
+        :point_a: First point of pair of points to find slope between
+        :point_b: Second point of pair of points to find slope between
+
+        """
+        return (ya - yb) / (xa - xb)
 
     def weigh_array(self, A, parts):
-        """ seperates an array into parts and ranks them from high to
-        low.
-        
-        :parts: the number of sections the list is to be split into
-        
+        """Separates an array into parts and ranks them from high to low.
+
+        :parts: The number of sections the list is to be split into
+
         """
-        
+
         for x in range(0 list.max):
-            step = len(A) % parts 
-            which_part = -1             #default part chose for the high
+            step = len(A) % parts
+            which_part = -1
             for x in range(0, parts):
-                if((a=max(A[0:len(A):step]), old_max): # for each part of the of the list find the max 
-                    old_max = a 
-                return() # the old max
-        
-        
-        return(speed,angle)
+                # For each part of the of the list find the max
+                if a == max(A[0:len(A):step], old_max):
+                    old_max = a
+                # TODO: Invalid and I don't know what you mean, @Kaleb
+                #return() # the old max
+        return (speed, angle)
 
     def move_multi(self):
         """Determines the direction to move the bot."""
@@ -107,12 +100,12 @@ class SimpleFollower(object):
 
     def sum_array(self, array, start, end):
         """Sums the values in a given range.
-        
+
         :param array: A list to sum.
         :param start: The starting point to sum from.
         :param end: The ending point to sum to.
         :returns: The sum of the values from start to end.
-        
+
         """
         val_sum = 0
         for x in range(start, end):

ad0b64a66f2786f86be3371ce95032dac49b2bfb
Jeff Schornick <jeff@schornick.org>
2014-01-10 18:19:50 -0500

    Update custom WiFi driver and related configs
    - version bone32 of custom 8192cu kernel module
    - make sure we depmod to catch new driver modules
    - 8192cu.conf: turn off power management
    - use wlan0 as the AP network device

--- os/install_bbb.sh
+++ os/install_bbb.sh
@@ -2,8 +2,11 @@
 
 echo "Updating packages..."
 apt-get purge udhcpd
-apt-get install hostapd dnsmasq
+apt-get install hostapd dnsmasq vim
 echo
 echo "Populating files..."
 rsync -va fs/* /
 
+echo "Running depmod..."
+# pick up new modules in /lib/modules
+depmod -a

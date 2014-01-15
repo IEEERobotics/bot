079f63eee4c64b5f5c39b4057c01e625f0170465
Jeff Schornick <jeff@schornick.org>
2014-01-14 20:57:25 -0500

    BBB configuration changes for new network adapters
    - remove custom Realtek 8192 driver and related hostap binary
    - enable 802.11n
    - enable masquerading and forwarding on boot
    - static IP for wired ethernet (192.168.1.1)

--- os/install_bbb.sh
+++ os/install_bbb.sh
@@ -2,11 +2,10 @@
 
 echo "Updating packages..."
 apt-get purge udhcpd
-apt-get install hostapd dnsmasq vim
+apt-get install hostapd dnsmasq
 echo
 echo "Populating files..."
 rsync -va fs/* /
 
-echo "Running depmod..."
-# pick up new modules in /lib/modules
-depmod -a
+update-rc.d masquerade defaults
+

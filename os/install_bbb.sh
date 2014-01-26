93bc8c1ec66d7e31320bfbe05706665af226ce68
Jeff Schornick <jeff@schornick.org>
2014-01-25 19:04:28 -0500

    Minor updates for WiFi networking during bootstrap

--- os/install_bbb.sh
+++ os/install_bbb.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-echo "Updating packages..."
-apt-get purge udhcpd
-apt-get install hostapd dnsmasq
-echo
-echo "Populating files..."
+echo "Populating filesystem..."
+chown -R root.root fs
 rsync -va fs/* /
+
+#cat /dev/null > /etc/udev/rules.d/70-persistent-net.rules
+
+echo
+echo Enabling uplink...
+ifup wlan1
+
+echo
+echo "Updating packages..."
+
+export DEBIAN_FRONTEND=noninteractive
+DPKG_OPTS=Dpkg::Options::="--force-confold"
+
+apt-get update
+apt-get -y purge udhcpd
+apt-get -y -o ${DPKG_OPTS} install hostapd dnsmasq
 
 update-rc.d masquerade defaults
 

c3f4561038c990e4615bf0008795e67e5d1ec5b0
Arpan Chakraborty <napratin@yahoo.co.in>
2014-02-11 22:32:57 -0500

    Corrected permissions for SSH key file

--- scripts/setup_bone.sh
+++ scripts/setup_bone.sh
@@ -54,7 +54,7 @@ then
         echo "Adding SSH keys..."
         mkdir -p ~/.ssh/
         wget -O ~/.ssh/id_rsa https://raw.github.com/NCSUhardware/bot/master/os/fs/root/.ssh/id_rsa?token=2446394__eyJzY29wZSI6IlJhd0Jsb2I6TkNTVWhhcmR3YXJlL2JvdC9tYXN0ZXIvb3MvZnMvcm9vdC8uc3NoL2lkX3JzYSIsImV4cGlyZXMiOjEzOTI3NzczMDl9--21a6fa04250b33c65ca853e4097e6524cc5f4917
-        chmod go-w ~/.ssh/id_rsa
+        chmod go-rwx ~/.ssh/id_rsa
         wget -O ~/.ssh/id_rsa.pub https://raw.github.com/NCSUhardware/bot/master/os/fs/root/.ssh/id_rsa.pub?token=2446394__eyJzY29wZSI6IlJhd0Jsb2I6TkNTVWhhcmR3YXJlL2JvdC9tYXN0ZXIvb3MvZnMvcm9vdC8uc3NoL2lkX3JzYS5wdWIiLCJleHBpcmVzIjoxMzkyNzc3MzgxfQ%3D%3D--4162dfcf86799a443bf7d23827484eb6cc0dded1
     fi
 fi

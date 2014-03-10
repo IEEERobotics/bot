82c29db177f79236d568341c3f68bb2d9b6aecae
Jeff Schornick <jeff@schornick.org>
2014-03-10 02:08:19 -0400

    Try to route through USB if no network during install

--- setup/setup_bone.sh
+++ setup/setup_bone.sh
@@ -79,8 +79,15 @@ echo "=================="
 if  /bin/ping -c1 -w1 google.com; then
     echo "Network is live"
 else
-    echo "Aborting setup due to lack of connectivty!"
-    exit 1
+    echo "No network connection detected, attempting to route via USB..."
+    route add default gw 192.168.7.1
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    if  /bin/ping -c1 -w1 google.com; then
+        echo "Network is temporarily live for install"
+    else
+        echo "Aborting setup due to lack of connectivty!"
+        exit 1
+    fi
 fi
 echo
 

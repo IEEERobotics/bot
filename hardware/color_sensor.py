20f6f50aae16453542fbcb12c1d2b9bff3b01b48
Ahmed Samara <asamara@ncsu.edu>
2014-03-13 19:51:25 -0400

    Moved green-biasing to raw data read

--- hardware/color_sensor.py
+++ hardware/color_sensor.py
@@ -96,6 +96,10 @@ class ColorSensor(I2CDevice):
             r = self.registers['RDATA'].read()
             g = self.registers['GDATA'].read()
             b = self.registers['BDATA'].read()
+
+            # Green is weakest color, so bias it to 110% reading.
+            g *= 1.1
+
             return valid, c, r, g, b
         else:
             return 1, 1, 1, 1, 1
@@ -155,8 +159,6 @@ class ColorSensor(I2CDevice):
         """
         diff_c, diff_r, diff_g, diff_b = self.get_percent_diff()
 
-        # Green is weakest color, so bias it slightly.
-        diff_g += 0.01
         if color == "green":
             if (diff_g > diff_r) and (diff_g +  > diff_b):
                 return True

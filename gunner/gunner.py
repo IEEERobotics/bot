42d7519b0dc12e720e122951553f3ddc04c92204
Jeff Schornick <jeff@schornick.org>
2014-03-13 00:37:00 -0400

    First attempt at ratio_localizer to handle arc

--- gunner/gunner.py
+++ gunner/gunner.py
@@ -6,6 +6,7 @@ import hardware.wheel_gun as wheel_gun
 import hardware.ultrasonic as ultrasonic
 import targeting_solver as targeting
 import time
+from math import acos, degrees
 
 class Gunner(object):
 
@@ -31,20 +32,70 @@ class Gunner(object):
         """Localize using ultrasonic sensors"""
         dists = self.ultrasonics.read_dists()
         # TODO: Handle skewed bot when not square to the course (e.g. on the arc line)
-        x, y, theta = self.dumb_localizer(dists)
+        x, y, theta = self.ratio_localizer(dists)
         self.logger.info("Localize calculated pose: ({}, {}, {})".format(
                     x, y, theta))
         return x,y,theta
 
     def dumb_localizer(self, dists_from_center):
-        """Localize based on range measurement from center of bot"""
+        """Localize based on range measurement from center of bot, assuming square"""
 
         self.logger.warning("Using dumb localizer!")
-        x = dists_from_center['left']
-        y = dists_from_center['front']
+        x = dists_from_center['back']
+        y = dists_from_center['left']
         theta = 0.0
-
+        self.logger.debug("Calculated pose: ({}, {}, {})".format(x,y,theta))
         return x, y, theta
+
+    def ratio_localizer(self, dists_from_center):
+        """Localize based on range measurement from center of bot"""
+
+        x_size = 1.2065  # in meters, official spec: 47.5"
+        y_size = 2.4257  # in meters, official spec: 95.5"
+
+        # bot will have left side facing target
+        # back + front = x_size when square
+        # NOTE: right sensor may be unreliable since it encouters the +x wall
+
+        dists = dists_from_center
+        x_tot = dists['back'] + dists['front']
+        ratio = x_size / x_tot
+        self.logger.debug("Ratio: {:0.3f}".format(ratio))
+        x_pos = dists['back'] * ratio
+        y_pos = dists['left'] * ratio
+        if ratio > 1.1:
+            # TODO: use longer sensor + front as alternate?
+            self.logger.error("Sensor X total ({}) MUCH smaller than course X ({})".format(x_tot, x_size))
+            raise ValueError
+        if ratio > 1:
+            self.logger.warning("Sensor X total ({}) smaller than course X ({}), assuming square".format(x_tot, x_size))
+            theta = 0.0
+        else:
+            # sin theta = (x_pos/back) = ratio
+            theta = degrees(acos(ratio))
+
+        # since the arc is well devined, assume we're always facing inward
+        if x_pos < (x_size/2.0):
+            theta = -theta
+
+        self.logger.debug("Calculated pose: ({}, {}, {})".format(x_pos,y_pos,theta))
+
+        return x_pos, y_pos, theta
+
+
+    def validate_pose(self, x_pos, y_pos, theta):
+
+        # minimums and maximums of possible blue block centers
+        y_min = 0.889  # 35"
+        y_max = 1.524  # 60"
+        x_min = 0.3    # ~12"
+        x_max = 1.07   # 42"
+        if not ((x_min < x_pos < x_max) and (y_min < y_pos < y_max)):
+            self.logger.warning("Invalid position for firing ({},{}), expected ({},{})-({},{})".format(
+                    x_pos, y_pos, x_min, y_min, x_max, y_max))
+            return False
+            logger.debug("Position is valid for firing")
+        return True
 
     @lib.api_call
     def aim(self):
@@ -55,6 +106,8 @@ class Gunner(object):
         time.sleep(0.1)
 
         x_pos, y_pos, theta = self.localize()
+        if not self.validate_pose(x_pos, y_pos, theta):
+            self.logger.error("What do I do now?!")
 
         # dart_velocity should be roughtly 5-14 m/s
         dart_velocity = self.gun.dart_velocity

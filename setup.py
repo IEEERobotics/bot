428808a3ee8001fa093ba42d03e327714990f992
Daniel Farrell <dfarrell@redhat.com>
2014-10-27 23:04:46 -0400

    Added basic tox testing setup
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
-import os
-from glob import glob
 from setuptools import setup, find_packages
-import unittest
 
-# Setup flags and parameters
-pkg_name = 'bot2014'  # top-level package name
-
-# Cache readme contents for use as long_description
-readme = open('README.md').read()
-
-# Call setup()
 setup(
-  name=pkg_name,
-  version='0.1',
-  description='NCSU IEEE Bot 2014',
-  long_description="NCSU IEEE Bot 2014",
-  author='NCSU IEEE',
-  author_email='group-ieee-robotics@ncsu.edu',
-  packages=find_packages(),
-  include_package_data=True,
-  scripts = glob("start.py"),
-  zip_safe=True,
-  install_requires=[
-    'pyDMCC'
-  ],
-  test_suite='tests',
-  platforms='any',
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Programming Language :: Python :: 2.7',
-  ])
+    name="bot",
+    version="1.0.1",
+    description="NCSU IEEE Robotics Team Codebase",
+    url="https://github.com/IEEERobotics/bot2014/",
+    author="NCSU IEEE Robotics",
+    author_email="dfarrell07@gmail.com",
+    # See for more classifer options: http://goo.gl/8jtDH4
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords="NCSU IEEE Robotics robot bot hardware",
+    packages=find_packages(),
+    # TODO: Fix for this style test layout
+    #test_suite="tests",
+    # See for more install_requires info: http://goo.gl/vCOKSw
+    # See for more install_requires info: http://goo.gl/5zQKIF
+    # TODO: List all known minimum requirements
+    install_requires=["pyyaml"],
+    # TODO: List dev and test env requirements
+    extras_require={
+        "dev": [],
+        "test": ["tox", "flake8"],
+    },
+    # TODO: Do we need to specify any package data?
+    # ^^See: https://github.com/pypa/sampleproject/blob/master/setup.py#L81
+    # TODO: Do we need to define an entry point?
+    # ^^See: https://github.com/pypa/sampleproject/blob/master/setup.py#L95
+)

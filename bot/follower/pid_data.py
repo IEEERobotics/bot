164c938325392552e07dc9aae8e3e9213b962bff
LizardChen <zchen10@ncsu.edu>
2014-12-20 18:31:30 -0500

    Add PID error preparation

---
+++ bot/follower/pid_data.py
@@ -0,0 +1,63 @@
+"""Preprocessing of IR sensor
+
+W is the width of IR sensor array
+
+L is the distance between the front and back sensor array
+
+d is the distance between two IR sensor
+
+N1 represents the first high reading value (from left to right) for front IR
+
+N2 represents the first high reading value (from left to right) for back IR
+
+n1 represents the relative coordinate for the front IR
+
+n2 represents the relative coordinate for the back IR """
+
+import sys
+
+from time import time
+
+class PID_data(object):
+
+    def __init__(self,d,l,w):
+        self.n1 = 0
+        self.n2 = 0
+        self.d  = d
+        self.l = l
+        self.w = w
+        self.ttheta=0
+#        self.theta = 0
+#        self.s = 0
+
+"""    def cal_value(self,N1,N2)
+        self.n1 = N1-4.5
+        self.n2 = N2-4.5
+        self.ttheta = (self.n1-self.n2)*self.d/self.l
+"""
+    def IR_index(self,IR)
+        i=0
+#        if (sum[IR]>=2)
+        if (IR[i]==0)
+            i=i+1
+        else (IR[i]=1)
+            N=i
+     
+        
+    def propa_error(self,N1,N2)
+        self.n1 = N1-4.5
+        self.n2 = N2-4.5
+        self.ttheta = (self.n1-self.n2)*self.d/self.l
+        theta = atan(self.ttheta)
+        return theta
+     
+    def rotate_error(self,N1,N2)
+        self.n1 = N1-4.5
+        self.n2 = N2-4.5
+        self.ttheta = (self.n1-self.n2)*self.d/self.l
+        s = abs(-self.l*self.ttheta-self.n2)/((1+self.ttheta^2)^0.5)
+        return s
+        
+        
+
+

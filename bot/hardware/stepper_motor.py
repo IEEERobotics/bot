33fe6c030b21db8e71cd86636956cd179dd4c8e7
Vijay Thiagarajan <vkthiaga@ncsu.edu>
2014-12-29 02:45:30 -0500

    Completed stepper motor abstraction. (Yet to be tested)

--- bot/hardware/stepper_motor.py
+++ bot/hardware/stepper_motor.py
@@ -9,132 +9,139 @@ class Stepper_motor(object):
 
     """Class for abstracting stepper motor settings."""
 
-    def __init__(self):
-        """Setup GPIO interface.
-        
-        """
-
-        # TODO(kvijay1995): After testing the motor, add logger,
-        # config for testing, and add pins to config.yaml
-        # Implement the helper functions.
-
-        # Build the GPIO pins in live mode
+    def __init__(self, gpio_1, gpio_2, gpio_3, gpio_4):
+        """Setup GPIO interface."""
 
         # Build logger
         self.logger = lib.get_logger()
 
-        self.gpio_1 = gpio.GPIO(8)
-        self.gpio_2 = gpio.GPIO(78)
-        self.gpio_3 = gpio.GPIO(76)
-        self.gpio_4 = gpio.GPIO(74)
+        # Load config
+        self.config = lib.get_config()
+
+        # This variable is inversely proportional to the speed
+        # of rotation. It is set to max speed by default.
+        self.time_int = 1.0
+
+        if self.config["testing"]["stepper"]:
+            # Get dir of simulated hardware files from config
+            gpio_test_dir = self.config['test_gpio_base_dir']
+
+            # Build gpio objects for BBB interaction, provide test dir
+            self.gpio_1 = gpio.GPIO(gpio_1, gpio_test_dir)
+            self.gpio_2 = gpio.GPIO(gpio_2, gpio_test_dir)
+            self.gpio_3 = gpio.GPIO(gpio_3, gpio_test_dir)
+            self.gpio_4 = gpio.GPIO(gpio_4, gpio_test_dir)
+
+        else:
+            # Build gpio pins
+            self.gpio_1 = gpio.GPIO(gpio_1)
+            self.gpio_2 = gpio.GPIO(gpio_2)
+            self.gpio_3 = gpio.GPIO(gpio_3)
+            self.gpio_4 = gpio.GPIO(gpio_4)
 
     def clockwise(self): 
         """The motor rotates clockwise.
 
-        TODO(kvijay1995) : Needs to be tested.
-
         """
         self.gpio_1.set_value(1)
         self.gpio_2.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_4.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(1)
         self.gpio_2.set_value(1)
         self.gpio_3.set_value(0)
         self.gpio_4.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(0)
         self.gpio_2.set_value(1)
         self.gpio_3.set_value(0)
         self.gpio_4.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(0)
         self.gpio_2.set_value(1)
         self.gpio_3.set_value(1)
         self.gpio_4.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_3.set_value(1)
         self.gpio_4.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_3.set_value(1)
         self.gpio_4.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_4.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_1.set_value(1)
         self.gpio_2.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_4.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
     def counter_clockwise(self): 
         """Rotates the motor counter clockwise
 
-        TODO(kvijay1995): Needs to be tested.
-
         """
         self.gpio_4.set_value(1)
         self.gpio_3.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_1.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(1)
         self.gpio_3.set_value(1)
         self.gpio_2.set_value(0)
         self.gpio_1.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(0)
         self.gpio_3.set_value(1)
         self.gpio_2.set_value(0)
         self.gpio_1.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(0)
         self.gpio_3.set_value(1)
         self.gpio_2.set_value(1)
         self.gpio_1.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_2.set_value(1)
         self.gpio_1.set_value(0)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_2.set_value(1)
         self.gpio_1.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(0)
         self.gpio_3.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_1.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
         self.gpio_4.set_value(1)
         self.gpio_3.set_value(0)
         self.gpio_2.set_value(0)
         self.gpio_1.set_value(1)
-        time.sleep(1.0/10000.0)
+        time.sleep(self.time_int/10000.0)
 
     @property
     def speed(self):
@@ -143,6 +150,7 @@ class Stepper_motor(object):
         :returns: Speed of the motor.
 
         """
+        return int(round((10000. - self.time_int)/100.)
 
     @speed.setter
     def speed(self, speed):
@@ -152,6 +160,15 @@ class Stepper_motor(object):
         :type speed: int
 
         """
+        if speed > 100:
+            self.logger.warning("Invalid speed {}, using 100%".format(speed))
+            speed = 100
+        elif speed < 0:
+            self.logger.warning("Invalid speed{}, using 0%".format(speed))
+            speed = 0
+
+        # Set time interval
+        self.time_int = 10001.0-(speed*100.)
 
     def rotate_90_clockwise(self):
         """Rotates the motor 90 degrees in the 
@@ -171,6 +188,11 @@ class Stepper_motor(object):
 
     @lib.api_call
     def test(self):
-        """Test function for the motor."""
-        self.rotate_90_clockwise()
-        self.rotate_90_counter_clockwise()
+        """Test function for the motor. Makes the
+        motor rotate clockwise until a
+        Keyboard interrupt. """
+        try:
+            while True:
+                self.clockwise()
+        except KeyboardInterrupt:
+            print "Done."

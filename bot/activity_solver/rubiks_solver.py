65c0bf15e8b69315539910895338a010ae3c9381
Ahmed Samara <asamara@ncsu.edu>
2015-03-09 20:39:57 -0400

    created rubiks solver stub

--- bot/activity_solver/rubiks_solver.py
+++ bot/activity_solver/rubiks_solver.py
@@ -10,6 +10,16 @@ class RubixSolver(object):
         self.logger = lib.get_logger()
 
         self.servo_pwm = self.config["rubiks"]["PWM"]["REV"]
-        self.rev_num = self.config["rubiks"]["GPIO"]["PWR"]
-        self.pwr_num = self.config["rubiks"]["GPIO"]["PWR"]
         
+        self.rev_num   = self.config["rubiks"]["GPIO"]["PWR"]
+        self.fwd_num   = self.config["rubiks"]["GPIO"]["FWD"]
+
+        self.pwr_num   = self.config["rubiks"]["GPIO"]["PWR"]
+        
+        # Build low-level things
+        self.gripper = servo_mod.Servo(self.servo_pwm)
+
+        # TODO(Remove as soon as this is functional)
+        self.gripper.test()
+
+

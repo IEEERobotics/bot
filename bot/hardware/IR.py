0c8ddee3b1f9167b3b9b3173ea484290251b423e
Jarrell Norwood <Jarrell Norwood>
2016-01-19 22:30:32 -0500

    Ir class

--- bot/hardware/IR.py
+++ bot/hardware/IR.py
@@ -3,22 +3,44 @@ from time import sleep
 
 #IRdict = {}
 
-s = smbus.SMBus(1)
-ms = [0 for i in xrange(20)]
+class IR():
+	""" interface for IR rangefinder."""
+	
+	def __init__(self):
+		self.hash_values = 
+	{"East Top" : 3, "East Bottom" : 4}
+		self.config = lib.config
+		
+		self.bus = smbus.SMBus(1)
+		#self.data = [0 for i in xrange(20)]
 
-def parse_lidar(msg):
-    first = msg[::2]
-    second = msg[1::2]
-    data = [second[i]* 256 + first[i] for i in xrange(len(first))]
-    return data
+	def parse_packets(self, msg):
+	"""
+		Return the 20 bytes of data from the IR Rangefinders.
+        :msg value: Desired motor velocity in velocity_range (ticks per sec).
+        :type value: int
+        :returns: Boolean value indicating success.
+	"""
+		first = msg[::2]
+		second = msg[1::2]
+		data = [second[i]* 256 + first[i] for i in xrange(len(first))]
+		return data
 
-while True:
-    for i in range(20):
-        ms[i] = s.read_byte(8)        
-    #IRdict[i] = 2000000*(raw_data**-1.55)
-    data = parse_lidar(ms)
-    data = [ ( (i**-1.55) *2000000 if i != 0 else 0 ) for i in data]
-    print data
-    sleep(1)
+	def read_values(self):
+	"""
+		read all 20 bytes
+	"""
+		for i in range(20):
+			ms[i] = s.read_byte(8)        
+		#IRdict[i] = 2000000*(raw_data**-1.55)
+		data = parse_lidar(ms)
+		data = [ ( (i**-1.55) *2000000 if i != 0 else 0 ) for i in data]
+		
+		return_dict = {}
+		for j in self.config["IR"]["active"]:
+			return_dict[j] = int(data[hash_values[j] -1])
+	
+		return return_dict
+		
 
 

2e48236e83a52f1d647129eaf7cef990f7ef458a
Vijay Thiagarajan <vkthiaga@ncsu.edu>
2016-01-21 20:51:22 -0500

    Corrected some bugs

--- bot/hardware/IR.py
+++ bot/hardware/IR.py
@@ -1,46 +1,36 @@
 import smbus
 from time import sleep
+import bot.lib.lib as lib
 
-#IRdict = {}
+class IR(object):
+    """ interface for IR rangefinder."""
 
-class IR():
-	""" interface for IR rangefinder."""
-	
-	def __init__(self):
-		self.hash_values = 
-	{"East Top" : 3, "East Bottom" : 4}
-		self.config = lib.config
-		
-		self.bus = smbus.SMBus(1)
-		#self.data = [0 for i in xrange(20)]
+    def __init__(self):
+        self.hash_values = {"East Top" : 3, "East Bottom" : 4}
+        self.config = lib.get_config()
+        self.bus = smbus.SMBus(1)
 
-	def parse_packets(self, msg):
-	"""
-		Return the 20 bytes of data from the IR Rangefinders.
-        :msg value: Desired motor velocity in velocity_range (ticks per sec).
-        :type value: int
+    def parse_packets(self, msg):
+        """	Return the 20 bytes of data from the IR Rangefinders.
+        :params msg:
+        :value: int
         :returns: Boolean value indicating success.
-	"""
-		first = msg[::2]
-		second = msg[1::2]
-		data = [second[i]* 256 + first[i] for i in xrange(len(first))]
-		return data
 
-	def read_values(self):
-	"""
-		read all 20 bytes
-	"""
-		for i in range(20):
-			ms[i] = s.read_byte(8)        
-		#IRdict[i] = 2000000*(raw_data**-1.55)
-		data = parse_lidar(ms)
-		data = [ ( (i**-1.55) *2000000 if i != 0 else 0 ) for i in data]
-		
-		return_dict = {}
-		for j in self.config["IR"]["active"]:
-			return_dict[j] = int(data[hash_values[j] -1])
-	
-		return return_dict
+        """
+        first = msg[::2]
+        second = msg[1::2]
+        data = [second[i]* 256 + first[i] for i in xrange(len(first))]
+        return data
+
+    def read_values(self):
+        for i in range(20):
+            ms[i] = s.read_byte(8)        
+        data = parse_lidar(ms)
+        data = [ ( (i**-1.55) *2000000 if i != 0 else 0 ) for i in data]
+        return_dict = {}
+        for j in self.config["IR"]["active"]:
+            return_dict[j] = int(data[hash_values[j] -1])
+        return 1
 		
 
 

1072d3540f30af11b98a18e36d7ec56118dfb070
Vijay Thiagarajan <vkthiaga@ncsu.edu>
2016-01-26 20:13:10 -0500

    Fixed some bugs with IR code

--- bot/hardware/IR.py
+++ bot/hardware/IR.py
@@ -6,7 +6,7 @@ class IR(object):
     """ interface for IR rangefinder."""
 
     def __init__(self):
-        self.hash_values = {"East Top" : 9, "East Bottom" : 8}
+        self.hash_values = {"East Top" : 9, "East Bottom" : 10}
         self.config = lib.get_config()
         self.bus = smbus.SMBus(1)
 
@@ -22,16 +22,16 @@ class IR(object):
         data = [second[i]* 256 + first[i] for i in xrange(len(first))]
         return data
 
-    @lib.api_call
     def read_values(self):
+        ms = range(20)
         for i in range(20):
-            ms[i] = s.read_byte(8)        
-        data = parse_lidar(ms)
+            ms[i] = self.bus.read_byte(8)        
+        data = self.parse_packets(ms)
         data = [ ( (i**-1.55) *2000000 if i != 0 else 0 ) for i in data]
         return_dict = {}
         for j in self.config["IR"]["active"]:
-            return_dict[j] = int(data[hash_values[j] -1])
-        return 1
+            return_dict[j] = int(data[self.hash_values[j] -1])
+        return return_dict
 		
 
 

82cb51fad6c24fb862e31deac7fd0a9b01dd0c8e
Ahmed Samara <asamara@ncsu.edu>
2016-02-02 20:00:55 -0500

    auto-install zbar

--- bot/setup/setup_bone.sh
+++ bot/setup/setup_bone.sh
@@ -2,7 +2,7 @@
 # Install standard software, do git configuration
 
 # Each should only contain the packages that are added at that level
-base="git python-yaml libzmq-dev python-zmq python-simplejson python-smbus python-virtualenv python-pip python-numpy python-dev build-essential vim vim-tiny less cmake pyyaml python-zmq libzbar-dev"
+base="git python-yaml libzmq-dev python-zmq python-simplejson python-smbus python-virtualenv python-pip python-numpy python-dev build-essential vim vim-tiny less cmake pyyaml python-zmq libzbar-dev unzip"
 extra="vim-nox ipython tmux screen nmap tree i2c-tools wireless-tools grc"
 
 while getopts aubh opt; do
@@ -200,7 +200,14 @@ python_module DMCC_Library
 python_module pypruss
 
 pip install sympy
-pip install zbar
+
+# Zbar doesn't work when installed from pip/apt-get, no idea why
+echo "Install zbar"
+wget https://github.com/npinchot/zbar/archive/master.zip
+unzip master.zip 
+cd zbar-master
+python setup.py install
+
 
 echo "==============="
 echo "Cleaining up..."

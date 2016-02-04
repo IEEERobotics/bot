7cc9854cb63dd5d5879220efd26334b6a8b46210
Ahmed Samara <asamara@ncsu.edu>
2016-02-04 13:49:26 -0500

    configured robot arm for image processing

--- bot/hardware/complex_hardware/robot_arm.py
+++ bot/hardware/complex_hardware/robot_arm.py
@@ -4,9 +4,15 @@
 import os
 import sympy
 from sympy import *
+
+import zbar
+from PIL import Image
+import cv2
+import numpy as np
+import time
+
 import bot.lib.lib as lib
 from bot.hardware.servo_cape import ServoCape
-
 
 class RobotArm(object):
 
@@ -22,7 +28,86 @@ class RobotArm(object):
             = ServoCape(self.bot_config["dagu_arm"]["servo_cape"])     
         # Empty list of zeros representing each joint   
         self.joints = [0]*5
+
+        # Image processing 
+        self.cam = cv2.VideoCapture(0)
+
+        # Camera dimensions had to be set manually
+        self.cam.set(3,1280)
+        self.cam.set(4,720)
+
+        # QR scanning tools.
+        self.scanner = zbar.ImageScanner()
+        scanner.parse_config('enable')
+
+        # Figure out what camera is being used
+        cam_model = arm_config["camera_model"]
+
+        # Constants based on calibration for image processing
+        self.cam_matrix  = np.float32(
+                        self.bot_config[cam_model]["camera_matrix"])
+        self.dist_coeffs = np.float32(
+                        self.bot_config[cam_model]["distance_coefficients"])
         
+        # initialize vertices of QR code
+        l = 1.5
+        self.qr_verts = np.float32([[-l/2, -l/2, 0],
+                            [-l/2,  l/2, 0],
+                            [ l/2, -l/2, 0],
+                            [ l/2,  l/2, 0]])
+         
+    @lib.api_call
+    def infinite_loop(self, frame):
+
+
+        while True:
+            ret, frame = self.cam.read()        
+            # Direct conversion cv -> zbar images did not work.
+            # Buffer file used to have native data structures.
+            cv2.imwrite('buffer.png', frame)
+
+            # PIL -> zbar
+            pil_im = Image.open('buffer.png').convert('L')
+            width, height = pil_im.size
+            raw = pil_im.tobytes()
+            z_im = zbar.Image(width, height, 'Y800', raw)
+            
+            # Find codes in image
+            self.scanner.scan(z_im)
+            for symbol in z_im:
+                tl, bl, br, tr = [item for item in symbol.location]
+                points = np.float32([[tl[0], tl[1]],
+                                     [tr[0], tr[1]],
+                                     [bl[0], bl[1]],
+                                     [br[0], br[1]]])
+
+                rvec, tvec = cv2.solvePnP(self.verts, points
+                                          , self.cam_matrix
+                                          , self.distcoeffs)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+    @lib.api_call
+    def draw_qr_on_frame(self, zbar_dat, draw_frame):
+
+        self.scanner.scan(zbar_dat)
+        for symbol in zbar_dat:
+            tl, bl, br, tr = [item for item in symbol.location]
+            points = np.float32([[tl[0], tl[1]],
+                                 [tr[0], tr[1]],
+                                 [bl[0], bl[1]],
+                                 [br[0], br[1]]])
+
+            cv2.line(frame, tl, bl, (100,0,255), 8, 8)
+            cv2.line(frame, bl, br, (100,0,255), 8, 8)
+            cv2.line(frame, br, tr, (100,0,255), 8, 8)
+            cv2.line(frame, tr, tl, (100,0,255), 8, 8)
+
+        return draw_frame
+
+
     @lib.api_call
     def grab(self):
         self.servo_cape.transmit_block([5] + [0,0,0,0,0])

a656f654f52b7402be73248309b2053c79439891
Duncan Page <dmpage318@gmail.com>
2016-03-03 20:10:52 -0500

    updated gbtest1

--- bot/hardware/complex_hardware/gbtest1.py
+++ bot/hardware/complex_hardware/gbtest1.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from robot_arm import RobotArm
 import bot.lib.lib as lib
+from time import sleep
 
 sensor_dist = 12.5
 
@@ -10,11 +11,18 @@ arm = RobotArm(lib.get_config('bot/config.yaml'))
 arm.reset_home_position()
 arm.rail.DisplacementMover(3600)
 
+sleep(.5)
+
 img = arm.cam.get_current_frame()
 offset = generic_blocks.get_lateral_offset(img, sensor_dist)[0]
-print 'moving 7th dof', offset, 'inches to the', 'left' if offset<0 else 'right'
+print 'need to move 7th dof', abs(offset), 'inches to the', 'left' if offset<0 else 'right'
+
+try: input('press enter to continue...')
+except: pass
 
 arm.rail.DisplacementMover(arm.rail.DisplacementConverter(-offset))
+
+sleep(.5)
 
 img = arm.cam.get_current_frame()
 x, y, z = generic_blocks.get_front_center(img)[0]

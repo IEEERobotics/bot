712d12afede0db0df0ed43935a0407262ea05335
Ahmed Samara <asamara@ncsu.edu>
2016-03-18 19:33:20 -0400

    corrected DMCC use

--- bot/hardware/complex_hardware/SeventhDOF.py
+++ bot/hardware/complex_hardware/SeventhDOF.py
@@ -14,8 +14,8 @@ class Rail_Mover:
         board_num = rail_motor_conf["board_num"]
         motor_num = rail_motor_conf["motor_num"]
         
-        self.rail_DMCC = pyDMCC.DMCC(board_num, motor_num, False)    
-        self.rail_motor = self.rail_DMCC[0]
+        self.rail_DMCC = pyDMCC.DMCC(board_num)    
+        self.rail_motor = self.rail_DMCC.motors[motor_num]
 
     def Orientor(self,Position):
         

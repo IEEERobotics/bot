e42a37061a89e7532f05482e855e99e7fb4282a4
Sean <smketrin@ncsu.edu>
2016-03-23 16:04:34 -0400

    fixing angles

--- bot/hardware/complex_hardware/robot_arm.py
+++ bot/hardware/complex_hardware/robot_arm.py
@@ -304,7 +304,7 @@ class RobotArm(object):
     
         LOOK = [0, 50, 155, 40, 180]
         time.sleep(1)
-        self.servo_cape.transmit_block([0] + self.HOME)
+        self.servo_cape.transmit_block([0] + LOOK)
         time.sleep(2)
         self.rail.DisplacementConverter(3.5)  #get the rail to the middle
         qr = self.rail_feedback()           #position infront of QRCode

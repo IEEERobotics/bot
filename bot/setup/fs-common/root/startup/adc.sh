cca92097413b5537fcf6d40a2e2b51a1cbdb675c
Sean <smketrin@ncsu.edu>
2016-04-25 22:14:42 -0400

    making changes

--- bot/setup/fs-common/root/startup/adc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=$(dirname $0)
-source $dir/slots.sh
-
-enable_adcs() {
-  load_slot cape-bone-iio
-}
-
--- bot/test_hardware/base_test.py
+++
@@ -1,8 +0,0 @@
-
-import bot.lib.lib as lib
-
-
-conf = lib.get_config()
-
-
-arm_config = 
